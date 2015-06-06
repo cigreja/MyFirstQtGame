c5cfab2b4842cd36db82b733f489defd8fdccf6a
cigreja <cigreja@live.com>
2015-06-06 10:57:37 -0400

    Move box up,down,left,right with QKeyEvent

--- myrect.cpp
+++ myrect.cpp
@@ -1,7 +1,19 @@
 #include "myrect.h"
-#include <QDebug>
+//#include <QDebug>
+#include <QKeyEvent>
 
 void MyRect::keyPressEvent(QKeyEvent *event)
 {
-    qDebug() << "MyRect knows that you pressed a key";
+    if (event->key() == Qt::Key_Left){
+        setPos(x()-10, y());
+    }
+    else if (event->key() == Qt::Key_Right){
+        setPos(x()+10, y());
+    }
+    else if (event->key() == Qt::Key_Up){
+        setPos(x(), y()-10);
+    }
+    else if (event->key() == Qt::Key_Down){
+        setPos(x(), y()+10);
+    }
 }
