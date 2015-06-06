4f38fa657ab4332742a15913ddb9399af4cf5f2d
cigreja <cigreja@live.com>
2015-06-06 10:16:53 -0400

    Added a Scene, Rectangle, and a View to main.cpp. Displays a box

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    // create a scene
+    QGraphicsScene * scene = new QGraphicsScene();
+
+    // create an item to put into the scene
+    QGraphicsRectItem * rect = new QGraphicsRectItem();
+    rect->setRect(0,0,100,100);
+
+    // add the item to the scene
+    scene->addItem(rect);
+
+    // create a view
+    QGraphicsView * view = new QGraphicsView(scene);
+    view->show();
 
     return a.exec();
 }
+
